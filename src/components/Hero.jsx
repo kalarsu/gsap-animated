22e79c1623855d1532357ca773b8c60960c6cc28
Kalar2025 <kalarsu@gmail.com>
2025-03-14 12:33:45 -0700

    update autoplay video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,7 +116,7 @@ const Hero = () => {
 
                     <video
                         src={getVideoSrc(currentIndex === totalVideos -1 ? 1: currentIndex)}
-                        //autoPlay
+                        autoPlay
                         loop
                         muted
                         className="absolute top-0 left-0 size-full object-cover object-center"
